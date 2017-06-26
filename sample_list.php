fbe83a231470afc1f1cfb0eef2513396107ec3db
Vatsala30 <vatsala3012@gmail.com>
2017-06-26 18:27:23 +0530

    Add files via upload

---
+++ sample_list.php
@@ -0,0 +1,32 @@
+<?php
+
+$books =  array(
+                
+    "Phil" => array("My girl" =>3, "The God Delusion" => 4,
+                    "Tweak" => 3, "The Shack" => 4,
+                    "The Birds In My Life" => 3,
+                    "New Moon" => 4),
+    
+    "Sameer" => array("The Last Lecture" => 3, "The God Delusion" => 4,
+                      "The Noble Wilds" => 3, "The Shack" => 4,
+                      "The Birds In My Life" => 3, "New Moon" => 1),
+    
+    "John" => array("A Thousand Splendid Suns" => 5, "The Secret" => 4,
+                    "Tweak" => 1),
+    
+    "Peter" => array("Chaos" => 5, "Php In Action" => 4),
+    
+    "Jill" => array("The Last Lecture" => 2, "The Secret" => 3,
+                    "The Noble Wilds" => 4, "The Host" => 4,
+                    "The World Without End" => 3, "New Moon" => 4),
+    
+    "Bruce" => array("The Last Lecture" => 3, "The Hollow" => 2,
+                     "The Noble Wilds" => 3, "The Shack" => 4,
+                     "The Appeal" => 2, "New Moon" => 3),
+    
+    "Tom" => array("Chaos" => 3)
+    
+    
+);
+
+?>

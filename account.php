bd8e82d90761eb1903002f2ee38f52ddb6527188
Vatsala30 <vatsala3012@gmail.com>
2017-06-26 18:24:35 +0530

    Add files via upload

---
+++ account.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<?php include('functions/functions.php') ?>
+<html>
+<head><title>My BookShop</title>
+<link href="style.css" rel="stylesheet" media="all"/>
+</head>
+<body>
+<div class="main_wrapper" style="background-color:orange;">
+
+<div class="header_wrapper"> 
+<img id="logo" src=""/>
+<h1>Book World!</h1>
+
+</div>
+<div class="content_wrapper">
+
+<div id="sidebar"style="color:orange;" >
+<p style="font-size:50px;text-align:center;"><b>W</br>E</br>L</br>C</br>O</br>M</br>E</br></b></p>
+
+ </div>
+ 
+
+
+<div id="content_area">
+
+</div>
+
+ <div id="form" style="background-color:orange; height:500px; width:500px; align:center;" >
+ <form method ="POST" action="rec_personalized.php">
+ Name: <input type ="text" name="user_name" placeholder="Hello Dear Shopper"/><br/>
+ <input type="submit" name="submit" value="submit"/>
+ </form>
+ </div>
+ </div> 
+<div id="footer"><h2 style="text-align:center; padding-top:30px;">&copy; 2017 www.bookworld.com</h2>  </div>
+
+
+</div>
+
+
+</div>
+
+
+
+
+</body>
+</html>
